a583cebed38b90264d4f048406075be09865b0a2
Wilson Wang <wilsonwang371@users.noreply.github.com>
2018-08-02 14:12:58 -0700

    update

--- src/pyexports.c
+++ src/pyexports.c
@@ -20,7 +20,7 @@ int create_yolo_handle(void **net, char ***names, const char *name_list,
         nnp_initialize();
     }
     *net = (void *)netp;
-    *names = get_labels(name_list);
+    *names = get_labels((char *)name_list);
     if (*names == NULL) {
         status = -1;
         goto errfreemasks;
